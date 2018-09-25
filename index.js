2e056b6323c9f585ea1da3b2041f16794db2a5ca
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-09-25 19:53:27 +0530

    updating

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports.cache = function (key, val) {
+    if (val) {
+        sessionStorage.setItem(key, JSON.stringify(val));
+        return val;
+    }
+    if (val === null) {
+        return sessionStorage.removeItem(key);
+    }
+    val = sessionStorage.getItem(key);
+    return val ? JSON.parse(val) : null;
+};
+
+module.exports.persist = function (key, val) {
+    if (val) {
+        localStorage.setItem(key, JSON.stringify(val));
+        return val;
+    }
+    var o = localStorage.getItem(key);
+    if (val === null) {
+        localStorage.removeItem(key);
+    }
+    return o ? JSON.parse(o) : null;
+};
